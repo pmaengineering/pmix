9407832290c96fab1650b2e445a1148d695e6d1c
joeflack4 <joeflack4@gmail.com>
2018-07-09 12:46:38 -0400

    Removed PPP sub-package and corresponding tests. Removed all test files, other than those required to pass remaining tests. Removed PPP from readme documentation and unnecessary commands from makefile.

--- pmix/ppp/config.py
+++
@@ -1,9 +0,0 @@
-"""Configuration settings for PPP package."""
-
-from jinja2 import Environment, PackageLoader
-
-
-TEMPLATE_ENV = Environment(loader=PackageLoader('pmix.ppp'), trim_blocks=True,
-                           lstrip_blocks=True)
-
-EXCLUSION_TOKEN = 'X'
